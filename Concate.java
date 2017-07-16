26eaded44e8fdb93a9673cccce3336ce7bc5b629
SANJANASRI-84284 <sanjusri962936@gmail.com>
2017-07-16 15:35:34 +0530

    Add files via upload

---
+++ Concate.java
@@ -0,0 +1,13 @@
+package GUVI;
+import java.util.*;
+public class Concate 
+{
+public static void main(String[] args)
+{
+	Scanner s=new Scanner(System.in);
+	String str=s.next();
+	String str2=s.next();
+	System.out.println(str+str2);
+	s.close();
+}
+}
